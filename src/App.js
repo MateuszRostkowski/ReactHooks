e4307bfee2939234540b579449ff75b10c32541b
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-12-05 22:18:58 +0100

    add simple useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      
+        <p>Pressed {count} times.</p>
+        <button onClick={() => setCount(count + 1)}>Press me</button>
+        <button onClick={() => setCount(0)}>Reset me</button>
     </div>
   );
 }
